e98de18e9f7e42a5e840333206ee1cf0e0202e65
toosriet-mac <tmtriet200800@toosriet.com>
2025-12-25 20:21:17 +0700

    update supabase key

--- t5-portal-fe/src/utility/supabaseClient.ts
+++ t5-portal-fe/src/utility/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://cqwfsrrxruuzobgplgxl.supabase.co";
-const SUPABASE_KEY =
-  "sb_publishable_qf_uXjoSy_JkPdyp6QS2Rg_Kwk1bWPD";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {

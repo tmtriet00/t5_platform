f03504b7862501b796098692d0e5bbc5801be1e5
Tran Minh Triet <tmtriet00@gmail.com>
2025-12-29 21:28:11 +0700

    refactor: Change IP address retrieval to use `CF-Connecting-IP` header and reformat the error response message.

--- t5-portal-fe/functions/_middleware.js
+++ t5-portal-fe/functions/_middleware.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     const country = context.request.cf.country;
-    const ipAddress = context.request.cf.ip;
+    const ipAddress = context.request.headers.get("CF-Connecting-IP");
 
     const isValidCountry = country === 'SG' || country === 'VN';
     const isValidIP = ["89.117.178.117"].includes(ipAddress);
@@ -9,7 +9,11 @@ export async function onRequest(context) {
         return context.next();
     }
 
-    return new Response(`Sorry, this application is not available to you. Country: ${country}, IP: ${ipAddress}`, {
+    return new Response(`
+        Sorry, this application is not available to you. 
+        Country: ${country}
+        IP: ${ipAddress}
+    `, {
         status: 403,
         headers: { 'Content-Type': 'text/plain' }
     });

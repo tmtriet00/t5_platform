efdbc033f2f6bab8ff6d770047dbfecaf2604317
Tran Minh Triet <tmtriet00@gmail.com>
2025-12-29 21:19:24 +0700

    feat: implement combined country and IP address validation for access and provide detailed denial message.

--- t5-portal-fe/functions/_middleware.js
+++ t5-portal-fe/functions/_middleware.js
@@ -1,11 +1,15 @@
 export async function onRequest(context) {
     const country = context.request.cf.country;
+    const ipAddress = context.request.cf.ip;
 
-    if (country === 'SG' || country === 'VN') {
+    const isValidCountry = country === 'SG' || country === 'VN';
+    const isValidIP = ["89.117.178.117"].includes(ipAddress);
+
+    if (isValidCountry && isValidIP) {
         return context.next();
     }
 
-    return new Response("Sorry, this application is only available in some countries.", {
+    return new Response(`Sorry, this application is not available to you. Country: ${country}, IP: ${ipAddress}`, {
         status: 403,
         headers: { 'Content-Type': 'text/plain' }
     });
